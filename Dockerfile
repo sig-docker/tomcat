46bc5b401b0d07bf1d1b40ff0d0bc0b9451faca3
Dan Boitnott <dboitnot@gmail.com>
2020-06-14 09:03:43 -0500

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+FROM tomcat:8.5.54-jdk8-openjdk
+
+RUN rm -Rf $CATALINA_HOME/webapps.dist \
+ && apt-get update -y  \
+ && apt-get install -y python-pip xtail \
+ && pip install ansible==2.9.2 lxml botocore boto3
+ && apt-get clean autoclean -y \
+ && apt-get autoremove -y \
+ && rm -rf /var/lib/apt/lists/* /root/.cache/pip/*
+
+COPY ansible /ansible/
+RUN mkdir -p /run.d \
+ && cd /ansible \
+ && mkdir -p galaxy \
+ && ansible-galaxy install --roles-path galaxy -r tomcat-requirements.yml --force
+
+COPY run.sh /run.sh
+
+EXPOSE 8080
+CMD /run.sh
---
+++ run.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+die () {
+	echo "ERROR: $*"
+	exit 1
+}
+
+[ -z "$TOMCAT_MEMORY_ARGS" ] && die "TOMCAT_MEMORY_ARGS not defined"
+
+for F in /run.d/*; do
+  echo "Sourcing $F ..."
+  . $F
+done
+
+cat >/ansible/group_vars/all/tomcat_dynamic.yml <<EOF
+tomcat_memory_args: "$TOMCAT_MEMORY_ARGS $TOMCAT_EXTRA_ARGS"
+tomcat_java_home: $JAVA_HOME
+EOF
+
+cd /ansible || die "failed to cd to /ansible"
+ansible-playbook tomcat-playbook.yml -t tomcat_conf --extra-vars "tomcat_root=$CATALINA_HOME" || die "ansible error"
+
+cd $CATALINA_HOME || die "failed to cd to CATALINA_HOME ($CATALINA_HOME)"
+bin/catalina.sh run
