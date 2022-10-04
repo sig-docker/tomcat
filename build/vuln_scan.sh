15c53495a1226757900fb6849be6fc6a44548f82
Dan Boitnott <dboitnot@gmail.com>
2022-10-04 09:02:12 -0500

    fix(gh-30): Disabled exit check from docker scan

--- build/vuln_scan.sh
+++ build/vuln_scan.sh
@@ -7,10 +7,10 @@ while true; do
     docker scan --json $1 |tee v.json
     RET=${PIPESTATUS[0]}
     echo "Scan return code: $RET"
-    if [ ! $RET -eq 0 ]; then
-       echo "Error in scan process."
-       exit 1
-    fi
+    # if [ !#  $RET -eq 0 ]; then
+    #    echo "Error in scan process."
+    #    exit 1
+    # fi
 
     echo "Checking results..."
     python3 build/check_scan_results.py scan_config.yml v.json 2> .vuln-res.tmp
