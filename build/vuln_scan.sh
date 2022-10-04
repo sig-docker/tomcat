698352b8432313e4a40666ac44a872a0e7ab2a7a
Dan Boitnott <dboitnot@gmail.com>
2022-10-04 08:55:29 -0500

    fix(gh-30): Improved vuln scan script

--- build/vuln_scan.sh
+++ build/vuln_scan.sh
@@ -5,12 +5,21 @@ NAP=5
 while true; do
     echo "Running vulnerability scan on $1"
     docker scan --json $1 |tee v.json
+    RET=${PIPESTATUS[0]}
+    echo "Scan return code: $RET"
+    if [ ! $RET -eq 0 ]; then
+       echo "Error in scan process."
+       exit 1
+    fi
 
     echo "Checking results..."
-    python3 build/check_scan_results.py scan_config.yml v.json |tee .vuln-res.tmp
+    python3 build/check_scan_results.py scan_config.yml v.json 2> .vuln-res.tmp
+    echo "--- STDERR ---"
+    cat .vuln-res.tmp
+    echo "--------------"
     RET=${PIPESTATUS[0]}
     echo "Check finished with $RET"
-    if [ $RET -eq 1 ] && grep -q 'JSONDecodeError' .vuln-res.tmp; then
+    if grep -q 'JSONDecodeError' .vuln-res.tmp; then
         echo "Check failed with malformed JSON. Retrying in $NAP seconds."
     else
         exit $RET
